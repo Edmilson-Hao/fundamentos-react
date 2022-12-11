15c27ffcb3c827ef3e675b48879b64e923e90d87
Edmilson-Hao <edmilsonwp@gmail.com>
2022-12-11 20:16:41 -0300

    Aula 27 - Repetição

--- src/components/repeticao/ListaAlunos.jsx
+++ src/components/repeticao/ListaAlunos.jsx
@@ -7,7 +7,7 @@ export default propr => {
     const alunosLI = alunos.map(aluno => {
         return (
             <li key={aluno.id}>
-                {aluno.id} - {aluno.nome} -> {aluno.nota}
+                {aluno.id} - {aluno.nome} - {aluno.nota}
             </li>
         )
     })
