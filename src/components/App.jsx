fdd932665ee7ec4d5d7043990c771fc699997cb9
Edmilson-Hao <edmilsonwp@gmail.com>
2022-12-11 10:56:31 -0300

    Aula 24 - Componente Card#2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,34 +6,39 @@ import Fragmento        from './basicos/Fragmento'
 import ComParametro     from './basicos/ComParametro'
 import Primeiro         from './basicos/Primeiro'
 
+import './App.css'
+
 //Quando default, não é necessário usar nome (pode ser função anônima)
 //export default function(props) {
 //Usar '_' underscore, quando a função tiver apenas 1 parametro
 //arrow function podem omitir parenteses e return()
 
 export default _ =>
-    <div id='app'>
+    <div className='App'>
         <h1>Fundamentos React</h1>
 
-        <Card titulo='#5 - Desafio Aleatório'>
-            <Aleatorio min={1} max={60} />
-        </Card>
+        <div className='Cards'>
 
-        <Card titulo='#4 - Fragmento'>
-            <Fragmento/>
-        </Card>
-        
-        <Card titulo='#2 - Com Parâmetro'>
-            <ComParametro
-                titulo='Situação do Aluno'
-                aluno='Pedro Silva'
-                nota={ 9.3 }
-            />
-        </Card>
+            <Card titulo='#5 - Desafio Aleatório'>
+                <Aleatorio min={1} max={60} />
+            </Card>
 
-        <Card titulo='#01 - Primeiro Componente'>
-            <Primeiro/>
-        </Card>
+            <Card titulo='#4 - Fragmento'>
+                <Fragmento/>
+            </Card>
+            
+            <Card titulo='#2 - Com Parâmetro'>
+                <ComParametro
+                    titulo='Situação do Aluno'
+                    aluno='Pedro Silva'
+                    nota={ 9.3 }
+                />
+            </Card>
+
+            <Card titulo='#01 - Primeiro Componente'>
+                <Primeiro/>
+            </Card>
+        </div>
     </div>
 
 /*
