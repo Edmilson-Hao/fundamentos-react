68c337a126be4e7ab5dc1996d1d0e5df7c008a83
Edmilson-Hao <edmilsonwp@gmail.com>
2022-12-13 05:59:49 -0300

    aula 41 - Desafio da megasena #1

---
+++ src/components/mega/Mega.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+
+import './Mega.css'
+
+export default props => {
+
+    const { qtde, min, max } = props
+
+    let arrayMega = []
+
+    const generateNumber = (min, max, array) => {
+        const randomNumber = parseInt((Math.random() * (max+1-min))+min)
+        return arrayMega.includes(randomNumber) ? generateNumber(min, max, array) : randomNumber
+    }
+
+    const generateSequence = qtde => {
+        const numbers = Array(qtde)
+            .fill(0)
+            .reduce((nums) => {
+                const novoNumero = generateNumber(min, max, nums)
+                return [...nums, novoNumero]
+            },[])
+            .sort((n1, n2) => n1 - n2)
+        return numbers
+    }
+
+    const generateList = (qtde, array) =>{
+        generateSequence(qtde)
+
+        const arrayList =  array.map((item, index) => {
+            <li>
+                {index+1}°: {item}
+            </li>
+        })
+
+        return arrayList
+    }
+
+    arrayMega.sort()
+
+    return (
+        <div>
+            {  }
+        </div>
+    )
+}
